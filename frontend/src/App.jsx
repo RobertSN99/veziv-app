e0e1747a207f8f6807ce21be19d3cf5c123479c6
Robert Spinoiu <robertsrn15@gmail.com>
2024-02-17 21:26:55 +0200

    update .gitignore, App.jsx, Navbar.jsx and main.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,24 +2,22 @@ import React from "react";
 import Home from "./pages/Home";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Projects from "./pages/Projects";
 import Contact from "./pages/Contact";
 
 function App() {
   return (
-    <>
+    <div className="h-screen">
       <Navbar />
-      <BrowserRouter>
         <Routes>
           <Route index element={<Home />} />
           <Route path="/home" element={<Home />} />
           <Route path="/projects" element={<Projects />} />
           <Route path="/contact" element={<Contact />} />
         </Routes>
-      </BrowserRouter>
       <Footer />
-    </>
+    </div>
   );
 }
 
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <nav className="w-screen bg-red-300 flex items-center justify-center">
+      <div className="flex-1 text-center">VEZIV</div>
+      <ul className="flex-1 flex items-center justify-center">
+        <li><Link to="/">Home</Link></li>
+        <li><a href="#">Resume</a></li>
+        <li><Link to="/projects">Projects</Link></li>
+        <li><Link to="/contact">Contact</Link></li>
+      </ul>
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
