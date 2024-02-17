391798be321fcfdb24aafe759e31a0de98d1a72d
Robert Spinoiu <robertsrn15@gmail.com>
2024-02-17 18:29:52 +0200

    added routes, created pages and components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,26 @@
-function App() {
+import React from "react";
+import Home from "./pages/Home";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Projects from "./pages/Projects";
+import Contact from "./pages/Contact";
 
+function App() {
   return (
     <>
-      Inside App.jsx
+      <Navbar />
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </BrowserRouter>
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
