dfc5d03b93c9724e1456f7038072ebab2d3c5628
Robert Spinoiu <robertsrn15@gmail.com>
2024-02-23 01:46:25 +0200

    update tailwind.config.js extended screen resolutions

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -10,6 +10,13 @@ export default {
         title: ['Bebas Neue'],
         body: ['Fredoka'],
         alternative: ['Karla']
+      },
+      screens: {
+        xs: "375px",
+        sm: "575px",
+        md: "768px",
+        lg: "992px",
+        xl: "1200px"
       }
     },
   },
