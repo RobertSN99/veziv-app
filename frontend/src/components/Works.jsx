5c3bbfe0e08cd3e5e29f05e0da01899a17fcac02
Robert Spinoiu <robertsrn15@gmail.com>
2024-02-23 22:24:45 +0200

    update Works.jsx temporarily removed responsiveness rearranged classes added comments

--- frontend/src/components/Works.jsx
+++ frontend/src/components/Works.jsx
@@ -27,15 +27,16 @@ const Works = () => {
   return (
     <section
       id="works"
-      className="bgWorks min-h-screen text-white flex flex-col gap-3 items-center justify-center py-5"
+      className="flex flex-col items-center justify-center min-h-screen gap-3 py-5 text-white bgWorks"
     >
       {placeholderData.map((project, index) => (
         <div
-          className="w-1/2 rounded-3xl my-5 tracking-widest bg-gradient-to-tr from-neutral-900 to-blue-950"
+          className="w-1/2 my-5 tracking-widest rounded-3xl bg-gradient-to-tr from-neutral-900 to-blue-950"
           key={index}
         >
           {index % 2 === 0 ? (
-            <div className="rounded-full xs:relative md:flex md:items-center md:justify-between">
+            <div className="flex items-center justify-between rounded-full">
+              {/* Displaying image first */}
               <div
                 className="relative overflow-hidden"
                 onMouseEnter={() => setHoveredIndex(index)}
@@ -44,42 +45,46 @@ const Works = () => {
                 <img
                   src={project.thumbnail}
                   alt=""
-                  className={`max-w-2xl rounded-3xl transition-all duration-300 relative xs:blur-sm ${
-                    hoveredIndex === index ? "md:blur-sm" : "md:blur-none"
+                  loading="lazy"
+                  className={`max-w-2xl rounded-3xl transition-all duration-300 relative ${
+                    hoveredIndex === index ? "blur-sm" : "blur-none"
                   }`}
                 />
-                <div className="bg-gradient-to-b from-transparent to-stone-900 absolute top-0 left-0 size-full rounded-sm" />
+                <div className="absolute top-0 left-0 rounded-sm bg-gradient-to-b from-transparent to-stone-900 size-full" />
                 <button
-                  className={`absolute rounded-full border-2 alternativebtn xs:opacity-1 xs:top-2/3 xs:left-1/2 xs:-translate-x-1/2 xs:px-2 xs:py-1 md:px-4 md:py-2 md:top-1/2 md:left-1/2 md:translate-x-[-50%] ${
-                    hoveredIndex === index ? "md:opacity-1" : "md:opacity-0"
+                  className={`absolute rounded-full border-2 alternativebtn px-4 py-2 top-1/2 left-1/2 translate-x-[-50%] ${
+                    hoveredIndex === index ? "opacity-1" : "opacity-0"
                   }`}
                 >
                   Project details
                 </button>
               </div>
-              <div className="flex flex-col items-center justify-center gap-3 text-center md:px-8 xs:absolute xs:top-0 xs:h-full xs:w-full md:w-auto md:static">
-                <h1 className="xs:text-xl md:text-3xl">{project.title}</h1>
+              {/* Displaying details */}
+              <div className="static flex flex-col items-center justify-center w-auto gap-3 px-8 text-center">
+                <h1 className="text-3xl">{project.title}</h1>
                 <a
                   href={project.customer}
                   target="_blank"
-                  className="rounded-full border-2 secondarybtn xs:px-2 xs:py-1 md:px-4 md:py-2"
+                  className="px-4 py-2 border-2 rounded-full secondarybtn"
                 >
                   Visit customer
                 </a>
               </div>
             </div>
           ) : (
-            <div className="rounded-full xs:relative md:flex md:items-center md:justify-between">
-              <div className="flex flex-col items-center justify-center gap-3 text-center z-[1] md:px-8 xs:absolute xs:top-0 xs:h-full xs:w-full md:w-auto md:static">
-                <h1 className="xs:text-xl md:text-3xl">{project.title}</h1>
+            <div className="flex items-center justify-between rounded-full">
+              {/* Displaying details first */}
+              <div className="static flex flex-col items-center justify-center w-auto gap-3 px-8 text-center">
+                <h1 className="text-3xl">{project.title}</h1>
                 <a
                   href={project.customer}
                   target="_blank"
-                  className="rounded-full border-2 secondarybtn xs:px-2 xs:py-1 md:px-4 md:py-2"
+                  className="px-4 py-2 border-2 rounded-full secondarybtn"
                 >
                   Visit customer
                 </a>
               </div>
+              {/* Displaying image */}
               <div
                 className="relative overflow-hidden"
                 onMouseEnter={() => setHoveredIndex(index)}
@@ -88,19 +93,20 @@ const Works = () => {
                 <img
                   src={project.thumbnail}
                   alt=""
-                  className={`max-w-2xl rounded-3xl transition-all duration-300 relative xs:blur-sm ${
-                    hoveredIndex === index ? "md:blur-sm" : "md:blur-none"
+                  loading="lazy"
+                  className={`max-w-2xl rounded-3xl transition-all duration-300 relative ${
+                    hoveredIndex === index ? "blur-sm" : "blur-none"
                   }`}
                 />
-                <div className="bg-gradient-to-b from-transparent to-stone-900 absolute top-0 left-0 size-full rounded-sm" />
-              </div>
-              <button
-                  className={`absolute rounded-full border-2 alternativebtn xs:opacity-1 xs:top-2/3 xs:left-1/2 xs:-translate-x-1/2 xs:px-2 xs:py-1 md:px-4 md:py-2 md:top-1/2 md:left-1/2 md:translate-x-[-50%] ${
-                    hoveredIndex === index ? "md:opacity-1" : "md:opacity-0"
+                <div className="absolute top-0 left-0 rounded-sm bg-gradient-to-b from-transparent to-stone-900 size-full" />
+                <button
+                  className={`absolute rounded-full border-2 alternativebtn px-4 py-2 top-1/2 left-1/2 translate-x-[-50%] ${
+                    hoveredIndex === index ? "opacity-1" : "opacity-0"
                   }`}
                 >
                   Project details
                 </button>
+              </div>
             </div>
           )}
         </div>
