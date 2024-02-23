d233e3e2cb6ac1cc8fca2ea4f79f03b9de22ac7b
Robert Spinoiu <robertsrn15@gmail.com>
2024-02-24 01:00:40 +0200

    feat: create ProjectItemGrid.jsx first commit of this component

---
+++ frontend/src/components/ProjectItemGrid.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const ProjectItemGrid = (item) => {
+  const data = {
+    thumbnail: item.thumbnail,
+    title: item.title,
+    customer: item.customer
+  }
+  return (
+    <div>ProjectItemGrid</div>
+  )
+}
+
+export default ProjectItemGrid
