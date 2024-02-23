61e49cfa5a0419bed6aafd1da23ec3f7221928ff
Robert Spinoiu <robertsrn15@gmail.com>
2024-02-24 01:01:15 +0200

    update frontend/src/index.css and Projects.jsx updated styles created Projects page

--- frontend/src/pages/Projects.jsx
+++ frontend/src/pages/Projects.jsx
@@ -1,9 +1,71 @@
-import React from 'react'
+import ProjectItemGrid from "../components/ProjectItemGrid";
+import ProjectItemList from "../components/ProjectItemList";
+import { FaList } from "react-icons/fa6";
+import { BsFillGrid3X3GapFill } from "react-icons/bs";
+import { CSSTransition } from "react-transition-group";
+import React, { useState } from "react";
 
 const Projects = () => {
+  const [selectedView, setSelectedView] = useState("list");
+  const handleViewSwitch = () => {
+    if (selectedView === "list") return setSelectedView("grid");
+    return setSelectedView("list");
+  };
+  const placeholderData = [
+    {
+      thumbnail: "../../assets/veziv.png",
+      title: "VEZIV Portfolio",
+      customer: "https://veziv.ro/",
+    },
+    {
+      thumbnail: "../../assets/netflix.png",
+      title: "Netflix Website",
+      customer: "https://netflix.com/",
+    },
+    {
+      thumbnail: "../../assets/pizzahut.png",
+      title: "Pizza Hut Website",
+      customer: "https://pizzahut.ro/",
+    },
+    {
+      thumbnail: "../../assets/bestjobs.png",
+      title: "BestJobs Platform",
+      customer: "https://bestjobs.eu/",
+    },
+  ];
   return (
-    <div>Projects</div>
-  )
-}
+    <main className="min-h-screen text-white pt-14 bg-neutral-950">
+      <div className="absolute right-5 top-14">
+        <button className="p-3 bg-blue-700 rounded-full" onClick={handleViewSwitch}>
+          {selectedView === "list" ? <FaList /> : <BsFillGrid3X3GapFill />}
+        </button>
+      </div>
+      <CSSTransition
+        in={selectedView === "list"}
+        timeout={300} // Adjust timeout duration as per your preference
+        classNames="view"
+        unmountOnExit
+      >
+        <div className="flex flex-col items-center justify-center w-full">
+          {placeholderData.map((project, index) => (
+            <ProjectItemList key={index} item={project} />
+          ))}
+        </div>
+      </CSSTransition>
+      <CSSTransition
+        in={selectedView === "grid"}
+        timeout={300} // Adjust timeout duration as per your preference
+        classNames="view"
+        unmountOnExit
+      >
+        <div className="flex items-center justify-center w-full">
+          {placeholderData.map((project, index) => (
+            <ProjectItemGrid key={index} item={project} />
+          ))}
+        </div>
+      </CSSTransition>
+    </main>
+  );
+};
 
-export default Projects
+export default Projects;
